22429d4d215d6a2bad8885005b9e37076a0e491c
AI kawagishi <ne201062@senshu-u.jp>
2023-06-02 15:01:04 +0900

    見た目変更

--- public/js/DrawChart.js
+++ public/js/DrawChart.js
@@ -26,8 +26,8 @@ function Main() {
         {
           label: "data1",
           data: dataArray,
-          borderWidth: 2,    // 線の太さ
-          borderColor: "rgba(0, 100, 0, 0.5)",    // 線の色
+          borderWidth: 7,    // 線の太さ
+          borderColor: "rgb(218, 114, 199)",    // 線の色
           fill: false   // 線の下の領域塗り潰しなし
         }
       ]
